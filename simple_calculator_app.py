8ce70b2d6ed9b7c87863bc0d72f32a48e72f0261
shawnsiate <shawnsiate84@gmail.com>
2026-05-07 03:26:52 +0800

    second commit, start

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+class Operation:
+    def __init__(self):
+        self.num1 = num1
+        self.num2 = num2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def calculate(self):
+        pass #mamaya
 
+class Addition(Operation):
+    pass
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Subtraction(Operation):
+    pass
 
+class Multiplication(Operation):
+    pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Division(Operation):
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def calculation():
+    try:
+    except ValueError:
+    except ZeroDivisionError:
+
+#since mdyo complicated pag nasa division beacuse gumagamit na ng float ganon.
